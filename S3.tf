6564f505a13bf64fc0a9e0c910b186656786d3cd
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2022-11-10 12:53:35 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -10,14 +10,4 @@ resource "aws_s3_bucket" "unrestricted" {
     }
 }
 
-resource "aws_s3_bucket" "unrestricted123" {
-  bucket = "examplebuckettftest"
-  acl    = "public-read-write"
-  versioning {
-    enabled = true
-  }
-  logging {
-    target_bucket = "pavantestbucket"
-    target_prefix = "log/"
-    }
-}
+ 
