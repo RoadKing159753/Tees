004cb57e357ccb08212abcac75121a129068fa86
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2022-11-24 18:34:28 +0530

    Update ec2file.tf

--- ec2file.tf
+++ ec2file.tf
@@ -14,10 +14,11 @@ provider "aws" {
 }
 
  
-resource "aws_instance" "app_server1" {
+resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
   tags = {
     Name = "ExampleAppServerInstance"
   }
 }
+
